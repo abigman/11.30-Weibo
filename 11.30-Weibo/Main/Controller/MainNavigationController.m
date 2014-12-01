4be9b6f2e5fc6e60f4feb98c93ab268241984689
yclzone <yclzone@gmail.com>
2014-12-01 10:43:25 +0800

    所有push到导航控制器的控制器，自动添加左右两个item，左边返回，右边回到主页

--- 11.30-Weibo/Main/Controller/MainNavigationController.m
+++ 11.30-Weibo/Main/Controller/MainNavigationController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainNavigationController.h"
+#import "UIBarButtonItem+ItemByImage.h"
 
 @interface MainNavigationController ()
 
@@ -24,6 +25,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/** 自定义push操作 */
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    [super pushViewController:viewController animated:YES];
+    NSLog(@"%@", self.viewControllers);
+    if (self.viewControllers.count > 1) {
+        // 调转后，自动隐藏底部bar
+        viewController.hidesBottomBarWhenPushed = YES;
+        
+        // 自动添加左右按钮
+        viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImageName:@"navigationbar_back" hightedImageName:@"navigationbar_back_highlighted" target:self action:@selector(back)];
+        viewController.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithImageName:@"navigationbar_more" hightedImageName:@"navigationbar_more_highlighted" target:self action:@selector(more)];
+    }
+}
+
+- (void)back {
+    [self popToRootViewControllerAnimated:YES];
+}
+
+- (void)more {
+    [self popToRootViewControllerAnimated:YES];
+}
+
 
 /*
 #pragma mark - Navigation
