066604dd818514021ea66b091e4c77701840252a
yclzone <yclzone@gmail.com>
2014-12-02 14:10:58 +0800

    UIImage 分类方法名注释

--- 11.30-Weibo/Category/UIImage+YCLImage.h
+++ 11.30-Weibo/Category/UIImage+YCLImage.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface UIImage (YCLImage)
+// iOS7 选择带 _os7 后缀的图片
 + (UIImage *)imageWithName:(NSString *)imageName;
+// 可拉伸图片
 + (UIImage *)resizableImageNamed:(NSString *)imageName;
 @end
