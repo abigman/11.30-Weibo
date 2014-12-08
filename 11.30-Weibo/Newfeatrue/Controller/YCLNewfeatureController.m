170cfd19ed1bd2215c7b9786dd4c6b78fe85228c
yclzone <yclzone@gmail.com>
2014-12-08 21:34:48 +0800

    新特性

---
+++ 11.30-Weibo/Newfeatrue/Controller/YCLNewfeatureController.m
@@ -0,0 +1,37 @@
+//
+//  YCLNewfeatureController.m
+//  11.30-Weibo
+//
+//  Created by Chenglin Yu on 14/12/8.
+//  Copyright (c) 2014年 yclzone. All rights reserved.
+//
+
+#import "YCLNewfeatureController.h"
+
+@interface YCLNewfeatureController ()
+
+@end
+
+@implementation YCLNewfeatureController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
