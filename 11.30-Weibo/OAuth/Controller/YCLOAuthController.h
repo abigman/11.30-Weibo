d0aeb62ff800342e6581f55f80f9640c580023b3
yclzone <yclzone@gmail.com>
2014-12-09 15:44:34 +0800

    添加授权控制器

---
+++ 11.30-Weibo/OAuth/Controller/YCLOAuthController.h
@@ -0,0 +1,13 @@
+//
+//  YCLOAuthController.h
+//  11.30-Weibo
+//
+//  Created by Chenglin Yu on 14/12/9.
+//  Copyright (c) 2014年 yclzone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCLOAuthController : UIViewController
+
+@end
