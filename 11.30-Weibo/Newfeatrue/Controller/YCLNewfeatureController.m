1aba63894bb4cfe1d679f3b8e883154187b1b477
yclzone <yclzone@gmail.com>
2014-12-11 00:27:25 +0800

    新特性控制器实现

--- 11.30-Weibo/Newfeatrue/Controller/YCLNewfeatureController.m
+++ 11.30-Weibo/Newfeatrue/Controller/YCLNewfeatureController.m
@@ -7,6 +7,8 @@
 //
 
 #import "YCLNewfeatureController.h"
+#import "UIView+YCLGeometry.h"
+#import "YCLMainTabBarController.h"
 
 @interface YCLNewfeatureController ()
 
@@ -17,11 +19,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setupScrollView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setupScrollView {
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:scrollView];
+    
+    CGFloat imageWidth = self.view.frameW;
+    CGFloat imageHeight = self.view.frameH;
+    
+    scrollView.contentSize = CGSizeMake(imageWidth * 4, imageHeight);
+    scrollView.pagingEnabled = YES;
+    
+    for (int i = 0; i<4; i++) {
+        UIImageView *imageView = [[UIImageView alloc] init];
+        imageView.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
+        imageView.frameX = i * imageWidth;
+        imageView.frameY = 0;
+        imageView.frameW = imageWidth;
+        imageView.frameH = imageHeight;
+        [scrollView addSubview:imageView];
+        
+        if (i == 4 - 1) {
+            // 添加进入微博按钮
+            
+            imageView.userInteractionEnabled = YES;
+            
+            UIButton *button = [[UIButton alloc] init];
+            button.backgroundColor = [UIColor redColor];
+            [button setTitle:@"开始体验" forState:UIControlStateNormal];
+            button.frameS = CGSizeMake(100, 20);
+            button.centerX = self.view.centerX;
+            button.centerY = self.view.frameH * 0.7;
+            [button addTarget:self action:@selector(gotoWeibo) forControlEvents:UIControlEventTouchUpInside];
+            [imageView addSubview:button];
+            
+            
+        }
+    }
+}
+
+- (void)gotoWeibo {
+    NSLog(@"----- 进入主页-----");
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    window.rootViewController = [[YCLMainTabBarController alloc] init];
 }
 
 /*
